3bfce6d5c6fcf8a21179e50e1467e43182bcca7f
cakici <seqragone-github@yahoo.de>
2013-07-11 20:47:51 +0200

    modified web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World4!');
+    var buffer = new Buffer(fs.readFileSync("index.html");
+    response.send(buffer.toString();
+    //response.send('Hello World4!');
 });
 
 var port = process.env.PORT || 5000;
